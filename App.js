db914e69d259b55b3486daf9c9280ba0f5eea1c7
Lyondealpha-hub <Alhajiahmed400@gmail.com>
2023-02-24 09:28:10 -0800

    feb24 closing

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {  Text, View } from 'react-native';
+import {  Text, View,Image,ImageBackground,ScrollView } from 'react-native';
 import TopNav from './components/TopNav';
+import user from './assets/user.png';
+import doc from './assets/doc.jpg';
+import doctor from './assets/doctor.png';
+import checkup from './assets/medical-checkup.png';
+import aid from './assets/first-aid.png';
+import lab from './assets/lab.png';
 
 export default function App() {
   return (
     <View className='container h-screen absolute' >
       <TopNav />
+      {/* Image render */}
+      <View className='contianer h-1/6'>
+        <ImageBackground resizeMode='cover' source={doc}  className='h-full flex  flex-row  mx-5 rounded-3xl py-1 '>
+          <View className='w-4/5 justify-center px-3'>
+            <Text className='text-xl  text-white items-center flex-wrap-reverse'>Looking For Specialist Doctor?</Text>
+            <Text className='text-white'>Join with an Online consultation</Text>
+          </View>
+        {/* <View className='w-2/5 items-end'>
+        <Image  className='justify-center items-center ' source={user} />
+        </View> */}
+        </ImageBackground>
+      </View>
+      {/* Category */}
+      <View className='w-full '>
+        <ScrollView horizontal={true} className='w-full' >
+          <View className=' flex flex-row px-5  w-full'>
+              <View className='w-1/4 px-5 bg-yellow-500'>
+                <Image source={doctor}></Image>
+
+              </View>
+
+              <View className='w-1/4 px-5 bg-gray-500'>
+                <Image source={checkup}></Image>
+              </View>
+
+              <View className='w-1/4 px-5 bg-purple-500'>
+              <Image source={aid}></Image>
+              </View>
+              <View className='w-1/4 bg-green-500'>
+              <Image source={lab}></Image>
+              </View>
+
+          
+
+          </View>
+        </ScrollView>
+      </View>
       <View className='flex-1 justify-center items-center  bg-red-500' >
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Oh yeah you there! </Text>
--- components/TopNav.js
+++ components/TopNav.js
@@ -1,18 +1,46 @@
 import React from 'react'
-import {  Text, View,Image } from 'react-native';
+import {  Text, View,Image, TextInput, StyleSheet } from 'react-native';
 import user from '../assets/user.png'
+import settings_cog from '../assets/settings-cog.png';
+import cart from '../assets/shopping-cart.png';
+import notification from '../assets/bell.png';
+import search from '../assets/search.png';
+
+import { Entypo, FontAwesome5,MaterialIcons,MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function TopNav() {
   return (
-    <View className='container flex flex-row  mt-9 justify-between items-center'>
-        <View className='w-1/2 h-20 justify-start pl-4 items-center bg-green-700 flex flex-row'>
+    <View>
+      <View className='container flex flex-row  mt-9 justify-between items-center'>
+        <View className='w-1/2  justify-start pl-4 items-center  flex flex-row'>
             <Image  className='justify-center items-center ' source={user} />
-            <Text className=' items-center'>Hello Good Morning!</Text>
+            {/* <Text className=' items-center'>Hello Good Morning!</Text> */}
         </View>
 
-        <View className='w-1/2 justify-end bg-yellow-400'>
-            <Text>Login me here</Text>
+        <View className='w-1/2 justify-end flex flex-row '>
+          <View className='w-3/5 justify-evenly flex flex-row '>
+            <Image  className='justify-center items-center ' source={notification} />
+            <Image  className='justify-center items-center ' source={cart} />
+            <Image  className='justify-center items-center ' source={settings_cog} />
+           
+          </View>
+           
         </View>
+    </View>
+    {/* Search bar  */}
+    <View className='contianer px-3 py-2 flex flex-row'>
+      <TextInput className='w-full h-10 border-2 rounded-2xl bg-slate-300 italic px-3' placeholder={'Search here'} > </TextInput>
+      <Image style={navStyles.search_icon}  className='justify-center items-center' source={search} />
+    </View>
     </View>
   )
 }
+
+
+const navStyles = StyleSheet.create({
+  search_icon: {
+    marginHorizontal:-45,
+    marginTop: 7,
+    
+  },
+})
