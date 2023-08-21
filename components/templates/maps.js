49f965e78086a62e7bddc2f9bb4718e05962cdac
Lyondealpha-hub <Alhajiahmed400@gmail.com>
2023-08-21 09:45:00 -0700

    changes

--- components/templates/maps.js
+++ components/templates/maps.js
@@ -1,31 +1,72 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from '@env'
 import MapView, { Marker } from 'react-native-maps';
 
 export default function MapsDirection() {
+
+    const [uselocations, setUselocations] = useState({
+        location: {},
+        description: '',
+        destination : '',
+    })
+
+    const updateStates = (key, value)=>{
+        setUselocations((prev)=>({...prev, [key]: value}))
+    }
+
+    console.log(uselocations)
+
+
+    // will use this chance to learn zustard state
+
+
     return (
         <View style={styles.container} className='container '>
             {/* Header */}
             <View style={styles.header} className='container h-full'>
                 {/* maps */}
-                <View className='h-full'>
+                <View className='h-full  '>
                     {/* trying to make the search hover on the map so the map fills the entire screeen  */}
-                    <View className='p-5 absolute top-0 left-0'>
+
+                      {/* map content */}
+
+
+                      <MapView
+                            className='flex-1 z-0'
+                            mapType='mutedStandard'
+                            initialRegion={{
+                                latitude: 51.5078788,
+                                longitude: -0.08773210000000001,
+                                latitudeDelta: 0.005,
+                                longitudeDelta: 0.005,
+                            }}
+                        />
+                    
                         <GooglePlacesAutocomplete
+                            className=''
                             placeholder='Where To'
                             styles={{
                                 container: {
-                                    flex: 0
+                                    flex: 0,
+                                    zIndex : 10,
+                                    position:'absolute',
+                                    width: '100%',
+                                    paddingTop: 10,
+                                    paddingHorizontal: 10,
+
                                 },
                                 textInput: {
-                                    fontSize: 18
+                                    fontSize: 18,
+                                    borderWidth: 1,
                                 }
                             }}
                             // currentLocation={true}
-                            onPress={(data, details) => { console.log(data, details) }}
+                            onPress={(data, details=null) => { console.log(data, details); updateStates('location', details.geometry.location, ) }}
                             enablePoweredByContainer={false}
+                            returnKeyType={'search'}
+                            fetchDetails={true}
                             minLength={2}
                             nearbyPlacesAPI="GooglePlacesSearch"
                             debounce={400}
@@ -34,24 +75,7 @@ export default function MapsDirection() {
                                 language: "en",
                             }}
                         />
-                    </View>
-
-                    {/* map content */}
-
-                    <View className='container h-full relative'>
-                        <MapView
-                            className='flex-1 z-10'
-                           
-                            initialRegion={{
-                                latitude: 37.78825,
-                                longitude: -122.4324,
-                                latitudeDelta: 0.0922,
-                                longitudeDelta: 0.0421,
-                            }}
-                        />
-                        
-                    </View> 
-
+            
                 </View>
 
 
@@ -80,13 +104,13 @@ const styles = StyleSheet.create({
     },
 
     header: {
-        flex: 7,
+        flex: 8,
         width: '100%',
         height: '100%',
     },
 
     footer: {
-        flex: 4,
+        flex: 3,
         borderTopLeftRadius: 15,
         borderTopRightRadius: 15,
         backgroundColor: 'lightgrey',
